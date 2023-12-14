d0b40652a3d5925606ed1209c0ab22804ebc9fab
dwarkesh2810 <dwarkeshpatel.siliconithub@gmail.com>
2023-12-14 13:46:49 +0530

    remove unnecessary code from validations also modify code

--- validations/validation.go
+++ validations/validation.go
@@ -8,35 +8,34 @@ import (
 	beego "github.com/beego/beego/v2/server/web"
 )
 
-func Init() {
-	validation.SetDefaultMessage(map[string]string{
-		"Required":     "Must be filled in",
-		"Min":          "Minimum allowed value %d",
-		"Max":          "Maximum allowed value %d",
-		"Range":        "Must be between %d and %d",
-		"MinSize":      "Minimum allowed length %d",
-		"MaxSize":      "Maximum allowed length %d",
-		"Length":       "Length must be %d",
-		"Alpha":        "Must consist of letters",
-		"Numeric":      "Must consist of numbers",
-		"AlphaNumeric": "Must consist of letters or numbers",
-		"Match":        "Must match %s",
-		"NoMatch":      "Must not match %s",
-		"AlphaDash":    "Must consist of letters, numbers or symbols (-_)",
-		"Email":        "Must be in correct email format",
-		"IP":           "Must be a valid IP address",
-		"Base64":       "Must be in correct base64 format",
-		"Mobile":       "Must be a valid mobile phone number",
-		"Tel":          "Must be a valid phone number",
-		"Phone":        "Must be a valid phone or mobile number",
-		"ZipCode":      "Must be a valid zip code",
-	})
-}
+// func Init() {
+// 	// validation.SetDefaultMessage(map[string]string{
+// 	// 	"Required":     "Must be filled in",
+// 	// 	"Min":          "Minimum allowed value %d",
+// 	// 	"Max":          "Maximum allowed value %d",
+// 	// 	"Range":        "Must be between %d and %d",
+// 	// 	"MinSize":      "Minimum allowed length %d",
+// 	// 	"MaxSize":      "Maximum allowed length %d",
+// 	// 	"Length":       "Length must be %d",
+// 	// 	"Alpha":        "Must consist of letters",
+// 	// 	"Numeric":      "Must consist of numbers",
+// 	// 	"AlphaNumeric": "Must consist of letters or numbers",
+// 	// 	"Match":        "Must match %s",
+// 	// 	"NoMatch":      "Must not match %s",
+// 	// 	"AlphaDash":    "Must consist of letters, numbers or symbols (-_)",
+// 	// 	"Email":        "Must be in correct email format",
+// 	// 	"IP":           "Must be a valid IP address",
+// 	// 	"Base64":       "Must be in correct base64 format",
+// 	// 	"Mobile":       "Must be a valid mobile phone number",
+// 	// 	"Tel":          "Must be a valid phone number",
+// 	// 	"Phone":        "Must be a valid phone or mobile number",
+// 	// 	"ZipCode":      "Must be a valid zip code",
+// 	// })
+// }
 
 func ValidErr(err []*validation.Error) []string {
 	message := make([]string, 0, len(err))
 	for i := range err {
-
 		message = append(message, err[i].Message)
 	}
 	return message
@@ -44,7 +43,6 @@ func ValidErr(err []*validation.Error) []string {
 
 func GetTag(err []*validation.Error) []string {
 	tags := make([]string, 0, len(err))
-
 	for i := range err {
 		tags = append(tags, err[i].Name)
 	}
@@ -58,9 +56,9 @@ func ValidationErrorResponse(c beego.Controller, err []*validation.Error) []stri
 		var errResponse string
 		switch Tags[i] {
 		case "Required", "Min", "Max", "Range", "MinSize", "MaxSize", "Length", "Match", "NotMatch":
-			errResponse = fmt.Sprintf(helper.LanguageTranslate(c, "validation."+Tags[i]), err[i].LimitValue)
+			errResponse = fmt.Sprintf("%s "+helper.LanguageTranslate(c, "validation."+Tags[i]), err[i].Field, err[i].LimitValue)
 		default:
-			errResponse = helper.LanguageTranslate(c, "validation."+Tags[i])
+			errResponse = fmt.Sprintf("%s "+helper.LanguageTranslate(c, "validation."+Tags[i]), err[i].Field)
 		}
 		errs = append(errs, errResponse)
 	}
